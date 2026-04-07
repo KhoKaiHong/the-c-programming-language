0b23d5e697c5b0d813736c11eba03888a913f2cc
Kho Kai Hong <khokaihong@gmail.com>
2026-04-07 21:37:39 +0800

    done e1-17

---
+++ chapter-1/e1-17.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// maximum line size
+#define MAXLINE 21
+// length threshold to cause a display
+#define THRESHOLD 10
+
+int getline(char line[], int limit);
+
+int main()
+{
+	char line[MAXLINE];
+	int length;
+
+	while ((length = getline(line, MAXLINE)) > 0) {
+		// if length of line provided exceeds threshold length (inclusive '\n', exclusive '\0'), print the line
+		if (length > THRESHOLD) {
+			printf("%3d - %s", length, line);
+		}
+	}
+}
+
+// getline implementation remains the same
+int getline(char line[], int limit)
+{
+	int i, j, c;
+
+	j = 0;
+
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
+		if (i < limit - 2) {
+			line[j] = c;
+			++j;
+		}
+	}
+
+	if (c == '\n') {
+		line[j] = c;
+		++j;
+		++i;
+	}
+
+	line[j] = '\0';
+
+	return i;
+}
